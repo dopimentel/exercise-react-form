90713209e746213e5bd43e1651f8c671bbe18303
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-09-04 17:02:19 -0300

    Requisito 2 - teste 2, 3

--- src/App.js
+++ src/App.js
@@ -5,24 +5,34 @@ import AddressForm from './components/AddressForm';
 class App extends React.Component {
   state = {
     name: '',
-    endereco: '',
+    address: '',
   };
 
   handleChange = ({ target }) => {
     const { name, value } = target;
     if (name === 'name') {
       const result = value.toUpperCase();
-      this.setState({ [name]: result });
-    } else this.setState({ [name]: value });
+      return this.setState({ [name]: result });
+    }
+    if (name === 'address') {
+      const result = this.validateAddress(value);
+      return this.setState({ [name]: result });
+    }
+    return this.setState({ [name]: value });
   };
 
+  // https://pt.stackoverflow.com/questions/91477/como-bloquear-caracteres-especiais-em-campo
+  // https://stackoverflow.com/questions/4374822/remove-all-special-characters-with-regexp
+
+  validateAddress = (address) => address.replace(/[^\w\s]/gi, '');
+
   render() {
-    const { name, endereco } = this.state;
+    const { name, address } = this.state;
     return (
       <>
         <h1>React Form</h1>
         <PersonalForm name={ name } handleChange={ this.handleChange } />
-        <AddressForm endereco={ endereco } handleChange={ this.handleChange } />
+        <AddressForm address={ address } handleChange={ this.handleChange } />
       </>
     );
   }
